2c277a5f8f99bd625313ed7d049472d3338129b2
lipeiwei <lipeiwei@dafy.com>
2020-03-22 16:51:26 +0800

    feat: 修改example，添加同时异步加载同个文件的情况

--- example/index.js
+++ example/index.js
@@ -3,7 +3,11 @@ const button = document.createElement('button')
 button.innerText = '点击获取异步脚本'
 button.onclick = function () {
   import('./print.js').then(exports => {
-    exports.print('this is a demo of aysnc load')
+    exports.print('this is a demo of aysnc load 11111111111111')
+  })
+
+  import('./print.js').then(exports => {
+    exports.print('this is a demo of aysnc load 22222222222222')
   })
 }
 
